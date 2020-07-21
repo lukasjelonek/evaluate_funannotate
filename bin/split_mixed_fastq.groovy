ba3cee5e98c2bf19fb0b45cfd883e1e0e5911197
Lukas Jelonek <lukas.jelonek@computational.bio.uni-giessen.de>
2020-07-21 13:57:49 +0200

    Add option to renumber the reads according to their position in the file

--- bin/split_mixed_fastq.groovy
+++ bin/split_mixed_fastq.groovy
@@ -7,6 +7,7 @@ import java.util.zip.GZIPInputStream
 import java.util.zip.GZIPOutputStream
 import java.nio.file.Files
 import java.nio.file.Paths
+import java.util.regex.Matcher
 import io.reactivex.rxjava3.core.*
 import groovy.transform.CompileStatic
 
@@ -19,6 +20,7 @@ def cli = new CliBuilder(
         )
 cli.f(longOpt:'fastq', args:1, argName: 'fastq', 'The fastq.gz file')
 cli.p(longOpt:'prefix', args:1, argName: 'prefix', 'The output prefix')
+cli.r(longOpt:'renumber', args:0, argName: 'renumber', 'Renumber the reads')
 cli.h(longOpt:'help', 'Show help')
 
 def options = cli.parse(args)
@@ -30,27 +32,44 @@ if (!options || options.h || !options.f || !options.p) {
 
 String file = options.f
 String prefix = options.p
-process(file, prefix)
+process(file, prefix, options.r)
+
+class Entry {
+    String id;
+    BufferedWriter writer;
+    int cur_line;
+}
 
 @CompileStatic
-void process(String file, String prefix) {
+void process(String file, String prefix, boolean renumber) {
     BufferedReader input = new BufferedReader(new InputStreamReader(new GZIPInputStream(Files.newInputStream(Paths.get(file)))))
 
-    Map<String, BufferedWriter> writeFiles = [:]
+    Map<String, Entry> writeFiles = [:]
 
     String cur_id = null
     input.eachLine{line, number ->
         if ((number-1) % 4 == 0) {
-            String[] s = line.split("/")
-            cur_id = s[-1]
-            line = line.replace(" ", "_")
+            Matcher m = line =~ /(.+)[ _](\d+)\/(\d+)$/
+            m.find()
+            String id = m.group(1)
+            String rn = m.group(2)
+            String pn = m.group(3)
+            cur_id = pn
+            if (!writeFiles.containsKey(cur_id)) {
+                def writer = new BufferedWriter(new OutputStreamWriter(new GZIPOutputStream(Files.newOutputStream(Paths.get(prefix + "_" + cur_id + ".fastq.gz")))))
+                writeFiles[cur_id] = new Entry('id': cur_id, 'writer': writer, cur_line:1)
+            } else {
+                writeFiles[cur_id].cur_line = writeFiles[cur_id].cur_line + 1
+            }
+            if (renumber) {
+                line = "${id}_${writeFiles[cur_id].cur_line}/${pn}"
+            } else {
+                line = "${id}_${rn}/${pn}"
+            }
         }
-        if (!writeFiles.containsKey(cur_id)) {
-            writeFiles[cur_id] = new BufferedWriter(new OutputStreamWriter(new GZIPOutputStream(Files.newOutputStream(Paths.get(prefix + "_" + cur_id + ".fastq.gz")))))
-        }
-        writeFiles[cur_id].println(line)
+        writeFiles[cur_id].writer.println(line)
     }
 
     input.close()
-    writeFiles.each{k,v -> v.flush(); v.close()}
+    writeFiles.each{k,v -> v.writer.flush(); v.writer.close()}
 }
